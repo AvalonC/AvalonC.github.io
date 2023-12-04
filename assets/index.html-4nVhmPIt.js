c28c55e64bbe1def22f0e4375cdeac33ab118a07
AvalonC <57788292+AvalonC@users.noreply.github.com>
2023-12-04 02:46:08 +0000

    Deploying to gh-pages from @ AvalonC/AvalonC.github.io@ec3f06948c13812620a364f19d457f1602de94e3 🚀

---
+++ assets/index.html-4nVhmPIt.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-1500d063","path":"/news/","title":"News","lang":"zh-CN","frontmatter":{"title":"News","article":false,"feed":false,"sitemap":false},"headers":[],"git":{},"readingTime":{"minutes":0,"words":1},"filePathRelative":null,"excerpt":""}');export{e as data};
---
+++ assets/index.html-ECOI9Nao.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./plugin-vue_export-helper-x3n3nnut.js";import{o,c as t,b as e}from"./app-LC0z0jEz.js";const l={},s=e("ul",null,[e("li",null,[e("a",{href:"Using_MIS"},"管理信息系统第十一版（Using MIS 11th Edition） David Kroenke, Randall Boyle")])],-1),_=[s];function c(i,r){return o(),t("div",null,_)}const f=n(l,[["render",c],["__file","index.html.vue"]]);export{f as default};
